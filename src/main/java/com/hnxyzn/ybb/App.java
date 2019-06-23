6db521e1718f32c9d9dd347d7fdb97dc27744d02
ybb <34953126@qq.com>
2019-06-23 17:21:00 +0800

    Initial commit

---
+++ src/main/java/com/hnxyzn/ybb/App.java
@@ -0,0 +1,18 @@
+package com.hnxyzn.ybb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Description springboot 启动入口
+ * @Author ybb 34953126@qq.com
+ * @Version V1.0.0
+ * @Since 1.0
+ * @Date 2019/6/23
+ */
+@SpringBootApplication
+public class App {
+    public static void main(String[] args) {
+        SpringApplication.run(App.class,args);
+    }
+}
